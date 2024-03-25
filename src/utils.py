17fcb4dcf1a4a1470ffa159bc843215e173263fd
psaboia <psaboiawork@gmail.com>
2024-03-25 19:46:21 -0400

    update nbs

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,11 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import requests
+import hashlib
+import concurrent.futures
+import os
+        
 
 # Change the image name pattern in this function
 def create_filename(x):
@@ -160,6 +165,14 @@ def filter_by_not_empty_column(df, column_name):
         raise ValueError("The column name is not in the dataframe")
     else:
         return df[df[column_name].notnull() & (df[column_name] != "")].copy()
+
+# Filter by not empty and not null column_name
+def filter_by_empty_column(df, column_name):
+    if column_name not in df.columns:
+        raise ValueError("The column name is not in the dataframe")
+    else:
+        return df[(df[column_name].isnull()) or (df[column_name] == "")].copy()
+    
     
 # Filter by 'unknown' column_name
 def filter_by_unknown_column(df, column_name):
@@ -186,3 +199,126 @@ def filter_by_no_value_column(df, column_name, value):
 def url_pad(file_location):
   return PAD_URL + str(file_location)
     
+
+def get_project_data(project_id):
+    import requests
+    import pandas as pd
+    import urllib3
+
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    API_URL = f"https://pad.crc.nd.edu/api/v2/projects/{project_id}/cards"
+    try:
+        # fetch_data_from_api
+        r = requests.get(url=API_URL,verify=False)  # NOTE: Using verify=False due to a SSL issue, I need a valid certificate, then I will remove this parameter.
+        r.raise_for_status() # Raise an exception if the status is not 200
+        data = r.json()
+        df = pd.json_normalize(data)
+        return df
+    except requests.exceptions.RequestException as e:
+        print(e)
+        print(f"Error accessing project data {project_id}: {r.status_code}")
+        return None
+
+def check_url(df):
+    # Initialize an empty DataFrame to store bad URLs and their status codes
+    bad_urls_df = pd.DataFrame(columns=['url', 'status_code'])
+    
+    # Check if the 'url' column has valid data
+    if 'url' in df:
+        # Check if URLs are formed correctly
+        for url in df['url']:
+            # Try to access the image
+            response = requests.head(url, verify=False)
+            if response.status_code != 200:
+                # If the URL is bad, add it to the DataFrame
+                bad_urls_df = bad_urls_df.append({'url': url, 'status_code': response.status_code}, ignore_index=True)
+    else:
+        print("DataFrame doesn't contain 'url' column.")
+        
+    # Return the DataFrame containing bad URLs
+    return bad_urls_df
+
+
+def check_hash(sample, output_folder):
+    filename = create_filename(sample)
+    filepath = os.path.join(output_folder, filename)
+    if os.path.exists(filepath):
+        if sample['image_hash'] == hashlib.md5(open(filepath,'rb').read()).hexdigest():
+            return True
+        else:
+            return False
+    else:
+        return False
+    
+def get_hash(sample, folder):
+    filename = create_filename(sample)
+    filepath = os.path.join(folder, filename)
+    #print(sample['id'], sample['url_status_code'], filepath)
+    if sample['url_status_code'] == 200:
+        if os.path.isfile(filepath):
+            hashlib_md5 = hashlib.md5(open(filepath,'rb').read()).hexdigest()
+            #print(hashlib_md5)
+            return hashlib_md5
+        else:
+            print(f"File not found {filepath}. Downloading...")
+            status_code = save_image_from_url(sample, folder)
+            if status_code == 200:
+                hashlib_md5 = hashlib.md5(open(filepath,'rb').read()).hexdigest()
+                return hashlib_md5
+            else:
+                print(f"Error downloading {sample['id']}. File not found {filepath}")
+                return None
+    else:        
+        return None
+
+
+def save_image_from_url(sample, output_folder):
+    r = requests.get(sample.url, verify=False)
+    output_path = os.path.join(
+        output_folder, create_filename(sample)
+    )
+    with open(output_path, "wb") as f:
+        f.write(r.content)
+        
+    status_code = r.status_code
+    r.close()
+    return status_code
+
+
+def delete_image(sample, folder):
+    filename = create_filename(sample)
+    filepath = os.path.join(folder, filename)
+    print(filepath)
+    if os.path.exists(filepath):
+        os.remove(filepath)
+
+
+def get_url_status_code(url):
+    r = requests.head(url, verify=False)
+    return r.status_code
+
+def get_hash_all(df, folder):    
+    column_exists = "url_status_code" in df.columns
+    
+    if ~column_exists:
+        df['url_status_code'] = df['url'].apply(lambda x: get_url_status_code(x))
+    
+    hash_codes = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_url = {
+            executor.submit(get_hash, sample, folder): sample for _, sample in df.iterrows()
+        }
+        for future in concurrent.futures.as_completed(
+            future_to_url
+        ):
+            sample = future_to_url[future]
+            try:               
+                #print(sample['id'], sample['url'], sample['url_status_code'] ,future.result())
+                hash_codes.append([sample['id'], sample['url_status_code'], future.result()])
+                
+            except Exception as exc:
+                print(
+                    "%s generated an exception: %s" % (sample['id'], exc)
+                )
+    return hash_codes
+
