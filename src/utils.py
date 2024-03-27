6a4ccf98f8e18b9ca9b466b93f48fda451af8094
psaboia <psaboiawork@gmail.com>
2024-03-27 14:43:23 -0400

    Fix connection issue

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,14 @@ import requests
 import hashlib
 import concurrent.futures
 import os
-        
+import logging
+import csv
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+# from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Change the image name pattern in this function
 def create_filename(x):
@@ -201,11 +208,8 @@ def url_pad(file_location):
     
 
 def get_project_data(project_id):
-    import requests
-    import pandas as pd
-    import urllib3
 
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    
     API_URL = f"https://pad.crc.nd.edu/api/v2/projects/{project_id}/cards"
     try:
         # fetch_data_from_api
@@ -322,3 +326,309 @@ def get_hash_all(df, folder):
                 )
     return hash_codes
 
+# Set up logging to file
+logging.basicConfig(filename='download_errors.log', level=logging.ERROR,
+                    format='%(asctime)s %(levelname)s:%(message)s')
+
+def download_file(url, filename, images_path):
+    """Download a file from a URL and save it to a local file."""
+    try:
+        response = requests.get(url, stream=True, verify=False)
+        if response.status_code == 200:
+            path = os.path.join(images_path, filename)
+            with open(path, 'wb') as f:
+                for chunk in response.iter_content(1024):
+                    f.write(chunk)
+            # print(f"File '{filename}' successfully downloaded to '{images_path}'")
+        else:
+            # Log error if the response status code is not 200
+            logging.error(f"Failed to download the file. URL: {url} returned status code: {response.status_code}")
+            raise Exception(f"Failed to download the file. URL: {url} returned status code: {response.status_code}")
+    except Exception as e:
+        # Log any other exceptions during the download process
+        logging.error(f"An error occurred while downloading the file: {e}")
+        # Optionally, you can re-raise the exception if you want it to be noticed by the calling function
+        raise
+
+
+def download_files_from_csv_file(file_path, images_path):
+    """Download files in parallel based on URLs from a CSV file with a progress bar."""
+    # Open the CSV file and parse its content
+    with open(file_path, newline='') as csvfile:
+        rows = list(csv.DictReader(csvfile)) # Convert to list for tqdm
+
+        # Initialize tqdm for the progress bar
+        pbar = tqdm(total=len(rows), desc="Downloading files")
+
+        def update(*args):
+            # Update the progress bar by one each time a file is downloaded
+            pbar.update()
+
+        # Use ThreadPoolExecutor to download files in parallel
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = []
+            for row in rows:
+                url = row['url']
+                filename = row['image_name']
+                
+                # check if filename is already downloaded
+                if os.path.exists(os.path.join(images_path, filename)):
+                    continue
+                  
+                # Schedule the download task
+                future = executor.submit(download_file, url, filename, images_path)
+                future.add_done_callback(update)
+                futures.append(future)
+
+            # Wait for all futures to complete
+            for future in futures:
+                future.result()
+
+        # Close the progress bar
+        pbar.close()
+
+
+
+##****************************************************************************##
+# Preprocessing functions: Extract RGB Information for FHI360
+##****************************************************************************##
+import os
+import csv
+import math
+import warnings
+import cv2 as cv
+import pandas as pd
+import urllib.request
+from datetime import datetime
+import ssl
+
+SAVE_DIR = './pixel_data/'
+REQS = {'LOG':'log.txt'}
+
+HORIZONTAL_BORDER = 12
+VERTICAL_BORDER = 0
+
+BLACK_THRESH_S = 35
+BLACK_THRESH_V = 70
+
+#!touch temp.png # Creates a temporary file for image processing
+with open('temp.png', 'a'):
+    pass
+
+#Takes a list of pixels and a BGR image and returns the average
+# Lab pixel values
+def px_avgPixelsLAB(pixels, img):
+  """Calculate the average Lab pixel values for a list of pixels in a BGR image."""
+  workingImg = cv.cvtColor(img, cv.COLOR_BGR2Lab)
+  totalL = 0
+  totalA = 0
+  totalB = 0
+  for pixel in pixels:
+    x = pixel[0]
+    y = pixel[1]
+    l, a, b = workingImg[x,y,:]
+    totalL += l
+    totalA += a
+    totalB += b
+  if len(pixels) != 0:
+    totalL /= len(pixels)
+    totalA /= len(pixels)
+    totalB /= len(pixels)
+  return int(totalL + 0.5), int(totalA + 0.5), int(totalB + 0.5)
+
+
+#Takes a list of pixels and a BGR image and returns the average
+# RGB pixel values
+def px_avgPixels(pixels, img):
+  """Calculate the average RGB pixel values for a list of pixels in a BGR image."""
+  totalB = 0
+  totalG = 0
+  totalR = 0
+  for pixel in pixels:
+    x = pixel[0]
+    y = pixel[1]
+    b,g,r = img[x,y,:]
+    totalB += b
+    totalG += g
+    totalR += r
+  if len(pixels) != 0:
+    totalB /= len(pixels)
+    totalG /= len(pixels)
+    totalR /= len(pixels)
+  return int(totalR + 0.5), int(totalG + 0.5), int(totalB + 0.5)
+
+
+#Takes a distance from a center and returns a weight between 0 and 1
+# determined by cosine such that a point at the cetner has weight 1,
+# and a point at the extremes has weight ~0.
+def intFind_cosCorrectFactor(dx, dy, centerX, centerY):
+  """Determine a weight between 0 and 1 based on distance from center, using a cosine function."""
+  relevantD = max((dx/centerX), dy/centerY)
+  relevnatDRads = (math.pi/2) * relevantD
+  return math.cos(relevnatDRads)
+
+#Takes a HSV image and returns a list of the most intense pixels in it,
+# after applying filtering to minimize black bars on the edges
+def intFind_findMaxIntensitiesFiltered(img):
+  """Return a list of pixels with maximum intensity, filtering out black bars on edges."""
+  imgS = img[:,:,1]
+  imgV = img[:,:,2]
+  maxI = 0
+  maxSet = []
+  centerX = imgS.shape[0]/2
+  centerY = imgS.shape[1]/2
+
+  for i in range(imgS.shape[0]):
+    dX = abs(centerX-i)
+    for j in range(imgS.shape[1]):
+      dY = abs(centerY-j)
+      sF = intFind_cosCorrectFactor(dX,dY,centerX,centerY)
+      cS = sF*imgS[i,j]
+      cV = sF*imgV[i,j]
+      if cS <= BLACK_THRESH_S and cV <= BLACK_THRESH_V:
+        pass
+      else:
+        maxSet.append((i,j))
+  return maxSet
+
+def fm_genIndex(regions, ColorList = ['R', 'G', 'B']):
+  """Generate an index for CSV column headers based on the number of regions and color list."""
+
+  index = ['Image', 'Contains', 'Drug %', 'PAD S#']
+  for letter in ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']:
+    for j in range(1,regions+1):
+      for color in ColorList:
+        tempStr = letter+str(j)+'-'+color
+        index.append(tempStr)
+  return index
+
+def fm_checkFormating(dir=SAVE_DIR, errorsFile=None):
+  """Check formatting and required files in the specified directory."""
+  if not os.path.isdir(dir):
+    os.mkdir(dir)
+  if errorsFile is None:
+    errors = open(dir+REQS['LOG'], 'a')
+  else:
+    errors = errorsFile
+  files = os.listdir(dir)
+  #print(files)
+  for item in REQS.values():
+    if item not in files:
+      errorString = str.format("Required file %s not found, creating.\n" %(item))
+      errors.write(errorString)
+      warnings.warn(errorString)
+      if item is REQS['LOG']:
+        temp = open(dir+item, 'w')
+        temp.close()
+      else:
+        os.mkdir(dir+item)
+  if errorsFile is None:
+    errors.close()
+
+def _regionGen(regions, region):
+  """Generate start and end points for a given region."""
+  start = 359
+  totalLength = 273
+  regionStart = start + math.floor(totalLength * (region/regions)) + VERTICAL_BORDER
+  regionEnd = start + math.floor(totalLength * ((region+1)/regions)) - VERTICAL_BORDER
+  return regionStart, regionEnd
+
+def _fullRoutine(img, roiFunc, df, RGB=True, regions=3):
+  """Complete routine for processing an image and extracting pixel information."""
+
+  letters = ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+  rList = []
+  gList = []
+  bList = []
+  imgHSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+  for lane in range(1,13):
+    laneStart = 17 + (53*lane)+HORIZONTAL_BORDER
+    laneEnd = 17 + (53*(lane+1))-HORIZONTAL_BORDER
+    letter = letters[lane-1]
+    for region in range(regions):
+      regionStart, regionEnd = _regionGen(regions, region)
+      roi = imgHSV[regionStart:regionEnd,laneStart:laneEnd,:]
+      rgbROI = img[regionStart:regionEnd,laneStart:laneEnd,:]
+      pixels = roiFunc(roi)
+      tempString = letter + str(region+1) + "-"
+      #Switches between RGB and Lab
+      if(RGB):
+        r, g, b = px_avgPixels(pixels, rgbROI)
+        df[tempString+'R'] = r
+        df[tempString+'G'] = g
+        df[tempString+'B'] = b
+      else:
+        l, a, blu = px_avgPixelsLAB(pixels, rgbROI)
+        df[tempString+'L'] = l
+        df[tempString+'a'] = a
+        df[tempString+'b'] = blu
+  return df
+
+
+def addIndex(runSettings):
+  """Add index information to run settings based on regions and color mode (RGB or Lab)."""
+  for setting in runSettings:
+    regions = runSettings[setting]['regions']
+    if(runSettings[setting]['RGB']):
+      runSettings[setting]['Index'] = fm_genIndex(regions)
+    else:
+      runSettings[setting]['Index'] = fm_genIndex(regions, ['L','a','b'])
+  return runSettings
+
+def regionRoutine(target, runSettings, save_dir=SAVE_DIR):
+  """Read a CSV file and process images according to the run settings."""
+
+  ssl._create_default_https_context = ssl._create_unverified_context
+  
+  startTime = datetime.now()
+  url = 'https://pad.crc.nd.edu'
+  dest = 'temp.png'
+  fm_checkFormating(save_dir)
+  errors = open(save_dir+REQS['LOG'], 'a')
+  #print("Starting...")
+  with open(target) as csvfile:
+    csvreader = csv.reader(csvfile, )
+    i = 0
+    next(csvreader)  # Skip the first line
+    for row in csvreader:
+      cTime = datetime.now()
+      i+=1
+      try:
+        urllib.request.urlretrieve(row[5], dest)
+        #urllib.request.urlretrieve(row[7], dest)
+        img = cv.imread(dest)
+        if (1250, 730, 3) != img.shape and (1220, 730, 3) != img.shape:
+          errorString = str.format("Error with file %s. Expected shape %s, found shape %s.\n" %(file, '(1250, 730, 3) or (1220, 730, 3)', str(img.shape)))
+          errors.write(errorString)
+          warnings.warn(errorString)
+        else:
+          for setting in runSettings:
+            data = {}
+            data = _fullRoutine(img, intFind_findMaxIntensitiesFiltered, data, runSettings[setting]['RGB'], runSettings[setting]['regions'])
+            #data['Image'] = row[0]
+            #data['Contains'] = row[1]
+            #data['Drug %'] = row[18]
+            #data['PAD S#'] = row[17]
+
+            data['Image'] = row[0]
+            data['Contains'] = row[2]
+            data['Drug %'] = row[3]
+            data['PAD S#'] = row[1]
+
+
+            df = pd.DataFrame(data, columns=runSettings[setting]['Index'], index=[data['Image']])
+            if(not os.path.exists(save_dir+setting)):
+              df.to_csv(save_dir+setting, mode='w', header=True)
+            else:
+              df.to_csv(save_dir+setting, mode='a', header=False)
+          elapsedTime = datetime.now() - cTime
+          # print("Finished image ",row[0]," in ",elapsedTime)
+      except Exception as e:
+        errorString = str.format("Error %s with file %s.\n" %(str(e), row[0]))
+        errors.write(errorString)
+        warnings.warn(errorString)
+      os.remove(dest)
+    errors.close()
+    endTime = datetime.now()
+    regions = 3+12+20
+    print('Time: ',endTime-startTime, ' time saved = ',i*regions*13/60.0)
